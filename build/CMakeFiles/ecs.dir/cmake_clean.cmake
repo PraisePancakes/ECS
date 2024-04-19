56d4066145e3dcc16719e789ff161d7e8d5a441f
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2024-04-19 17:34:44 -0400

    first commit

---
+++ build/CMakeFiles/ecs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecs.dir/src/main.cpp.obj"
+  "CMakeFiles/ecs.dir/src/main.cpp.obj.d"
+  "ecs.exe"
+  "ecs.exe.manifest"
+  "ecs.pdb"
+  "libecs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
